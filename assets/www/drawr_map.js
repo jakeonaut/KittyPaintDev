579d999c2102472d91a74173b2fc64d23426ae68
katzen <ben.1plus1@gmail.com>
2014-03-22 15:08:50 -0500

    border drawring fix

--- assets/www/drawr_map.js
+++ assets/www/drawr_map.js
@@ -52,7 +52,37 @@ DrawrMap.prototype.isChunkLoaded = function(chunk_numx, chunk_numy){
     }
 }
 
+  
 DrawrMap.prototype.addPoint = function(x,y,brush){
+    var gamex = Math.floor(x/PER_PIXEL_SCALING); // convert to ingame (big) pixels
+    var gamey = Math.floor(y/PER_PIXEL_SCALING);
+    
+    var chunks_affected = this.getChunksAffected(gamex, gamey, brush);
+    var chunks_local_coords = this.getChunkLocalCoordinates(gamex, gamey, chunks_affected, brush);
+    
+    var chunks_written = []; // store the chunks already written to, to avoid redundancy
+    
+    for(var i=0; i<4; ++i){
+        if(chunks_affected[i] && chunks_local_coords[i]){
+            var chunk_numx = chunks_affected[i].x;
+            var chunk_numy = chunks_affected[i].y;
+            var chunk_written_id = chunk_numx + ":" + chunk_numy;
+            if(chunks_written.indexOf(chunk_written_id) < 0){
+                if(this.isChunkLoaded(chunk_numx, chunk_numy)){
+                    var chunk = this.chunks[chunk_numx][chunk_numy];
+                    chunk.addPoint(chunks_local_coords[i].x, chunks_local_coords[i].y, brush);
+                }else{
+                    console.log("Chunk not loaded: (" + chunk_numx + ", " + chunk_numy + ")");
+                }
+                chunks_written.push(chunk_written_id);
+            }
+        }
+    }
+    
+}
+
+DrawrMap.prototype.oldAddPoint = function(x,y,brush){
+    // DEPRECATED - will incorrectly draw near the edge of chunks
     // find where to add to chunk
     var gamex = Math.floor(x/PER_PIXEL_SCALING); // convert to ingame (big) pixels
     var gamey = Math.floor(y/PER_PIXEL_SCALING);
@@ -61,19 +91,63 @@ DrawrMap.prototype.addPoint = function(x,y,brush){
     var chunk_localx = gamex % CHUNK_BLOCK_SIZE; // pixel location in chunk local coordinates
     var chunk_localy = gamey % CHUNK_BLOCK_SIZE; 
     
-    /*
-     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-     WHJAT 
-     IF THE FBRUISH IS ON THE EDGE OF ONE OF THE CHUNKS UGGGGGGGGGGGGGGGGGGGGGGH ADFGASDFASDF WHATEVER
-     SSSSASASASDFASDFASDFASDFASDFASDF
-    */
-    
     if(this.isChunkLoaded(chunk_numx, chunk_numy)){
         var chunk = this.chunks[chunk_numx][chunk_numy];
         chunk.addPoint(chunk_localx, chunk_localy, brush);
     }else{
         console.log("Chunk not loaded: (" + chunk_numx + ", " + chunk_numy + ")");
     }
+}
+
+DrawrMap.prototype.getChunkLocalCoordinates = function(gamex, gamey, chunk_nums_affected, brush){
+    // calculate pixel location in local coordinates of each of the 4 possible chunks.
+    // getChunksAffected will always return in this order: topleft, bottomleft, topright, bottomright 
+    // Preserve this order in this return
+    // this function will probably explode if brush.size > CHUNK_BLOCK_SIZE. that should never happen.
+    
+    var chunk_general_localx = gamex % CHUNK_BLOCK_SIZE; // these are correct for the chunk where the *CENTER OF THE BRUSH* is
+    var chunk_general_localy = gamey % CHUNK_BLOCK_SIZE; 
+    
+    var chunk_numx = Math.floor(gamex / CHUNK_BLOCK_SIZE); // calculate which chunk the *CENTER OF THE BRUSH* is in
+    var chunk_numy = Math.floor(gamey / CHUNK_BLOCK_SIZE);
+    
+    var chunk_local_coords = [];
+    for(var i=0; i<4; ++i){
+        if(chunk_nums_affected[i]){
+            var dx = chunk_numx - chunk_nums_affected[i].x;
+            var dy = chunk_numy - chunk_nums_affected[i].y;
+            chunk_local_coords.push({x: chunk_general_localx + dx * CHUNK_BLOCK_SIZE,
+                                     y: chunk_general_localy + dy * CHUNK_BLOCK_SIZE }); // this is beautiful
+        }else{
+            chunk_local_coords.push(0);
+        }
+        
+    }
+    return chunk_local_coords;
+}
+
+DrawrMap.prototype.getChunksAffected = function(gamex, gamey, brush){
+    // To find chunks affected: find 1 or more chunks for each 4 points of the square mask of the brush
+    // getChunksAffected will always return in this order: topleft, bottomleft, topright, bottomright
+    // if one of those 4 chunks isn't loaded, log it, and its location in the return array will be null
+    
+    var chunks_found = [];
+    var brush_delta = brush.size/2;
+    // coordinates of the 4 coordinates of the brush, in the correct order
+    var brush_xs = [gamex - brush_delta, gamex - brush_delta, gamex + brush_delta, gamex + brush_delta];
+    var brush_ys = [gamey - brush_delta, gamey + brush_delta, gamey - brush_delta, gamey + brush_delta];
+    
+    for(var i=0; i<4; ++i){
+        var chunk_numx = Math.floor(brush_xs[i] / CHUNK_BLOCK_SIZE); // calculate which chunk this (ingame) pixel is in
+        var chunk_numy = Math.floor(brush_ys[i] / CHUNK_BLOCK_SIZE);
+        if(this.isChunkLoaded(chunk_numx, chunk_numy)){
+            chunks_found.push({x: chunk_numx, y: chunk_numy});
+        }else{
+            chunks_found.push(0); // preserve the order of the chunks in the return value!
+            console.log("Chunk not loaded: (" + chunk_numx + ", " + chunk_numy + ")");
+        }
+    }
+    return chunks_found;
 }
 
 DrawrMap.prototype.draw = function(ctx){
