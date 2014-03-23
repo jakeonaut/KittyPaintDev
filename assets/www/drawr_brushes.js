2c248e50358aa6b3ae84c0b77b7697333c8896d8
Jake Trower <jakeonaut@gmail.com>
2014-03-22 19:25:11 -0500

    Added white brushes and fixed colored brush creation

--- assets/www/drawr_brushes.js
+++ assets/www/drawr_brushes.js
@@ -3,60 +3,58 @@
 
 function DrawrBrushes(onload_continuation){
     this.brushes = [];
-    this.blackfilenames = ["circle1.png", "circle4.png","circle8.png","circle16.png","circle32.png","kappa32.png","cathead32.png","bar8.png"];
-	this.orangefilenames = ["orangecircle1.png", "orangecircle4.png","orangecircle8.png","orangecircle16.png","orangecircle32.png","orangekappa32.png","orangecathead32.png","orangebar8.png"];
-	this.filenames = this.blackfilenames;
+    this.filenames = ["circle1", "circle4","circle8","circle16","circle32","kappa32","cathead32","bar8"];
+	this.brushnames = this.filenames;
+	this.colors = ["black", "orange", "white"];
     this.filewidths = [1,4,8,16,32,32,32,8];
 	this.numrealbrushes = this.filenames.length;
     this.selected_brush = 0;
     this.loaded_count = 0;
 
-    for(var i=0; i<this.filenames.length; ++i){
-        var temp_img = new Image();
-        temp_img.src = "brushes/" + this.filenames[i];
-        var brush_obj = {img: temp_img, name: this.filenames[i], size: this.filewidths[i], loaded: 0};
-        
-        var self_ref = this;
-        temp_img.onload = function(){
-            brush_obj.loaded = 1;
-            self_ref.loaded_count++;
-            if(self_ref.loaded_count == self_ref.filenames.length){
-                onload_continuation();
-            }
-        }
-        this.brushes.push(brush_obj);
-    }
-	this.filenames = this.orangefilenames
-	for(var i=0; i<this.filenames.length; ++i){
-        var temp_img = new Image();
-        temp_img.src = "brushes/" + this.filenames[i];
-        var brush_obj = {img: temp_img, name: this.filenames[i], size: this.filewidths[i], loaded: 0};
-        
-        var self_ref = this;
-        temp_img.onload = function(){
-            brush_obj.loaded = 1;
-            self_ref.loaded_count++;
-            if(self_ref.loaded_count == self_ref.filenames.length){
-                onload_continuation();
-            }
-        }
-        this.brushes.push(brush_obj);
-    }
+	for(var j=0; j<this.colors.length; ++j){
+		var color = this.colors[j];
+		for(var i=0; i<this.filenames.length; ++i){
+			var temp_img = new Image();
+			temp_img.src = "brushes/" + color + "/" + this.filenames[i] + ".png";
+			var brush_obj = {
+				img: temp_img, 
+				name: this.filenames[i], 
+				color: color, 
+				size: this.filewidths[i], 
+				loaded: 0
+			};
+			
+			var self_ref = this;
+			temp_img.onload = function(){
+				brush_obj.loaded = 1;
+				self_ref.loaded_count++;
+				if(self_ref.loaded_count == self_ref.filenames.length){
+					onload_continuation();
+				}
+			}
+			this.brushes.push(brush_obj);
+		}
+	}
 }
 
 DrawrBrushes.prototype.getBrushes = function(){
-    return this.brushes.slice(0, this.numrealbrushes);
+    return this.brushes;
+}
+
+DrawrBrushes.prototype.getBrushNames = function(){
+	return this.brushnames;
+}
+
+DrawrBrushes.prototype.getColors = function(){
+    return this.colors;
 }
 
 DrawrBrushes.prototype.selectBrush = function(brush, color){
     if(brush + '' == (1*brush) + ''){ // if brush by number
         this.selected_brush = 1*brush;
     }else{
-        var stripfiletype = function(x){return x.indexOf('.') >= 0 ? x.substr(0, x.indexOf('.')) : x;};
-        brush = stripfiletype(brush);
-		brush = color + brush;
         for(var i=0; i<this.brushes.length; ++i){
-            if(stripfiletype(this.brushes[i].name) == brush){
+            if(this.brushes[i].name == brush && this.brushes[i].color == color){
                 this.selected_brush = i;
                 return this.selected_brush;
             }
