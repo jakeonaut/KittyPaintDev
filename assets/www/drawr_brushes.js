503e3612e2a9cfcc1a190f58da18390d66e17568
katzen <ben.1plus1@gmail.com>
2014-03-30 23:12:18 -0500

    fixed a lot of things with editing brushes

--- assets/www/drawr_brushes.js
+++ assets/www/drawr_brushes.js
@@ -22,7 +22,9 @@ function DrawrBrushes(onload_continuation){
 	this.selected_brush = 0;
     this.brush_size = 1;
 	this.size_variations = [1, 4, 8, 16, 32];
-    this.loaded_count = 0;
+    
+    this.count_to_load = 0;
+    this.count_loaded = 0;
     
 	//load brushes and stamps
 	for(var i=0; i<this.brush_names.length; ++i){
@@ -32,11 +34,12 @@ function DrawrBrushes(onload_continuation){
 			if (type == "brush"){
 				var sized_images = [];
 				var temp_img = new Image();
-				temp_img.src = "brushes/" + name + "/" + name + ".png";
+				temp_img.src = "brushes/" + name + "/" + name + ".png"; // this image is never used now? i think
 				var brush_obj = {
 					img: temp_img,
 					name: name + j,
-					size: 0,
+					size: 0, // is this used?
+                    sizes: [],
 					sized_images: [],
 					color: this.named_colors[j],
 					type: type,
@@ -44,25 +47,33 @@ function DrawrBrushes(onload_continuation){
 				}
 				for (var k = 0; k<this.size_variations.length;++k){
 					var temp_img = new Image();
+                    this.count_to_load++; // new image to load
 					var size = this.size_variations[k];
 					temp_img.src = "brushes/"+name+"/"+size+".png"; //brushes/circle/0/16.png
 					sized_images.push(temp_img);
+                    brush_obj.sizes.push(size);
 					
 					var self_ref = this;
-					temp_img.onload = function(){
-						brush_obj.loaded += 1;
-						if (brush_obj.loaded == 1)
-							self_ref.loaded_count++;
-						if (self_ref.loaded_count == self_ref.brush_names.length){
-							onload_continuation();
-						}
-					}
+                    
+                    // create a closure for the continuation -- pass the context
+                    (function(i,j,k,size,brush_obj,temp_img){
+                        temp_img.onload = function(){
+                            var c = brush_obj.color;
+                            brush_obj.sized_images[k] = DrawrBrushes.setImageColor(temp_img, size, c.r, c.g, c.b);
+                            brush_obj.loaded += 1;
+                            self_ref.count_loaded++;
+                            if (self_ref.count_loaded == self_ref.count_to_load){
+                                onload_continuation();
+                            }
+                        };
+                    })(i,j,k,size,brush_obj,temp_img);
 				}
 				brush_obj.sized_images = sized_images;
 				this.brushes.push(brush_obj);
 			}
 			else if (type == "stamp"){
 				var temp_img = new Image();
+                this.count_to_load++; // new image to load
 				temp_img.src = "brushes/" + name + "/" + j + ".png";
 				var brush_name = name + j;
 				
@@ -79,12 +90,11 @@ function DrawrBrushes(onload_continuation){
 				var self_ref = this;
 				temp_img.onload = function(){
 					brush_obj.loaded = 1;
-					self_ref.loaded_count++;
-					
-					if(self_ref.loaded_count == self_ref.brush_names.length){
-						onload_continuation();
-					}
-				}
+                    self_ref.count_loaded++;
+                    if (self_ref.count_loaded == self_ref.count_to_load){
+                        onload_continuation();
+                    }
+				};
 				this.brushes.push(brush_obj);
 			}
 		}
@@ -126,32 +136,59 @@ DrawrBrushes.prototype.setBrushSize = function(size){
 	this.brush_size = size;
 }
 
+DrawrBrushes.setImageColor = function(img, size, r, g, b){
+    var ctemp = document.createElement('canvas');
+    ctemp.width = size;
+    ctemp.height = size;
+    var ctxtemp = ctemp.getContext("2d");
+    
+    ctxtemp.drawImage(img, 0, 0);
+    var img_data = ctxtemp.getImageData(0, 0, ctemp.width, ctemp.height);
+    for (var i = 0; i < img_data.data.length; i+= 4){
+        img_data.data[i] = r;
+        img_data.data[i+1] = g;
+        img_data.data[i+2] = b;
+    }
+    ctxtemp.putImageData(img_data,0,0);
+    
+    var imgnew = new Image();
+    imgnew.src = ctemp.toDataURL("image/png");
+    return imgnew;
+}
+
+DrawrBrushes.setBrushColor = function(brush, r, g, b){
+    if(r && g && b){
+        if(r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255) return 0;
+        brush.color.r = r;
+        brush.color.g = g;
+        brush.color.b = b;
+    }else{
+        brush.color.r = r.r;
+        brush.color.g = r.g;
+        brush.color.b = r.b;
+    }
+    
+    for(var k=0; k<brush.sizes.length; ++k){
+        var img = DrawrBrushes.setImageColor(brush.sized_images[k], brush.sizes[k], brush.color.r, brush.color.g, brush.color.b);
+        brush.sized_images[k] = img;
+    }
+}
+
 DrawrBrushes.draw = function(ctx, x, y, brush, size){
+    //console.trace();
 	var s = Math.floor(size/2);
 	if (brush.type == "brush"){
-		index = 0;
+		/*index = 0;
 		if (size == 4) 			index = 1;
 		else if (size == 8) 	index = 2;
 		else if (size == 16) 	index = 3;
-		else if (size == 32) 	index = 4;
+		else if (size == 32) 	index = 4;*/
+        var index = brush.sizes.indexOf(size);
 		
-		var ctemp = document.createElement('canvas');
-		ctemp.width = size; ctemp.height = size; 
-		var ctxtemp = ctemp.getContext("2d");
-		var brush_img = brush.sized_images[index];
-		
-		ctxtemp.drawImage(brush_img, 0, 0);
-		var img_data = ctxtemp.getImageData(0, 0, ctemp.width, ctemp.height);
-		for (var i = 0; i < img_data.data.length; i+= 4){
-			img_data.data[i] = brush.color.r;  		//rreed
-			img_data.data[i+1] = brush.color.g;		//green
-			img_data.data[i+2] = brush.color.b;		//bblue
-		}
-		ctxtemp.putImageData(img_data,0,0);
-		
-		var img = new Image();
-		img.src = ctemp.toDataURL("imgage/png");
-		ctx.drawImage(img, x-s, y-s, size, size);
+        if(index >= 0){
+            var brush_img = brush.sized_images[index];
+            ctx.drawImage(brush_img, x-s, y-s, size, size);
+        }
 	}else if (brush.type == "stamp"){
 		var brush_img = brush.img;
 		
